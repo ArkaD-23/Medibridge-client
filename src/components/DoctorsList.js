06a2036f873f2652072f5e79fbd519428ec5fab4
Arkaprava Dhar <raj2003dhar@gmail.com>
2025-08-26 08:51:27 +0530

    fix: connection to vm api

--- src/components/DoctorsList.js
+++ src/components/DoctorsList.js
@@ -21,7 +21,7 @@ export default function DoctorsList() {
 
   const fetchDoctors = async () => {
 
-    const res = await fetch("http://gateway:8090/v1/users/doctors", {
+    const res = await fetch("http://4.247.161.61:8090/v1/users/doctors", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
